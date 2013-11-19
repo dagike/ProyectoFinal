6d38d9bc6c9c267db71be1ed6eb34d5c15b85edf
dagike <sinkstillfree@gmail.com>
2013-11-18 19:19:26 -0600

    ClientePanel corregido

--- src/vistas/principal/AgregarClientePanel.java
+++ src/vistas/principal/AgregarClientePanel.java
@@ -1,18 +1,11 @@
 package vistas.principal;
 
-import javax.swing.JPanel;
-import java.awt.GridBagLayout;
-import javax.swing.JLabel;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
-import javax.swing.JTextField;
-import java.awt.Color;
-import javax.swing.JRadioButton;
-import javax.swing.ButtonGroup;
+import javax.swing.*;
+import java.awt.*;
+
 
 public class AgregarClientePanel extends JPanel {
 	private JTextField tFNombreCompleto;
-	private JTextField tFDireccion;
 	private JTextField tFEmail;
 	private ButtonGroup btnGroupTarjetas = new ButtonGroup();
 	private JTextField tFNumeroTarjeta;
@@ -20,10 +13,10 @@ public class AgregarClientePanel extends JPanel {
 	public AgregarClientePanel() {
 		setBackground(Color.WHITE);
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{112, 180, 0, 30, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 30, 0, 30, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.columnWidths = new int[]{135, 289, 0, 30, 0};
+		gridBagLayout.rowHeights = new int[]{40, 0, 0, 0, 0, 0, 30, 0, 30, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
 		
 		JLabel lblNombreCompleto = new JLabel("Nombre Completo");
@@ -43,29 +36,31 @@ public class AgregarClientePanel extends JPanel {
 		add(tFNombreCompleto, gbc_tFNombreCompleto);
 		tFNombreCompleto.setColumns(10);
 		
+		JTextArea tADireccion = new JTextArea();
+		tADireccion.setLineWrap(true);
+		tADireccion.setWrapStyleWord(true);
+		GridBagConstraints gbc_tADireccion = new GridBagConstraints();
+		gbc_tADireccion.gridheight = 3;
+		gbc_tADireccion.insets = new Insets(0, 0, 5, 5);
+		gbc_tADireccion.fill = GridBagConstraints.BOTH;
+		gbc_tADireccion.gridx = 1;
+		gbc_tADireccion.gridy = 2;
+		add(new JScrollPane(tADireccion), gbc_tADireccion);
+		
 		JLabel lblDireccion = new JLabel("Direccion");
 		GridBagConstraints gbc_lblDireccion = new GridBagConstraints();
 		gbc_lblDireccion.anchor = GridBagConstraints.EAST;
 		gbc_lblDireccion.insets = new Insets(0, 0, 5, 5);
 		gbc_lblDireccion.gridx = 0;
-		gbc_lblDireccion.gridy = 2;
+		gbc_lblDireccion.gridy = 3;
 		add(lblDireccion, gbc_lblDireccion);
-		
-		tFDireccion = new JTextField();
-		GridBagConstraints gbc_tFDireccion = new GridBagConstraints();
-		gbc_tFDireccion.insets = new Insets(0, 0, 5, 5);
-		gbc_tFDireccion.fill = GridBagConstraints.HORIZONTAL;
-		gbc_tFDireccion.gridx = 1;
-		gbc_tFDireccion.gridy = 2;
-		add(tFDireccion, gbc_tFDireccion);
-		tFDireccion.setColumns(10);
 		
 		JLabel lblEmail = new JLabel("e-mail");
 		GridBagConstraints gbc_lblEmail = new GridBagConstraints();
 		gbc_lblEmail.anchor = GridBagConstraints.EAST;
 		gbc_lblEmail.insets = new Insets(0, 0, 5, 5);
 		gbc_lblEmail.gridx = 0;
-		gbc_lblEmail.gridy = 3;
+		gbc_lblEmail.gridy = 5;
 		add(lblEmail, gbc_lblEmail);
 		
 		tFEmail = new JTextField();
@@ -73,16 +68,17 @@ public class AgregarClientePanel extends JPanel {
 		gbc_tFEmail.insets = new Insets(0, 0, 5, 5);
 		gbc_tFEmail.fill = GridBagConstraints.HORIZONTAL;
 		gbc_tFEmail.gridx = 1;
-		gbc_tFEmail.gridy = 3;
+		gbc_tFEmail.gridy = 5;
 		add(tFEmail, gbc_tFEmail);
 		tFEmail.setColumns(10);
 		
 		JRadioButton rdbtnVisa = new JRadioButton("Visa");
 		btnGroupTarjetas.add(rdbtnVisa);
 		GridBagConstraints gbc_rdbtnVisa = new GridBagConstraints();
+		gbc_rdbtnVisa.anchor = GridBagConstraints.EAST;
 		gbc_rdbtnVisa.insets = new Insets(0, 0, 5, 5);
 		gbc_rdbtnVisa.gridx = 0;
-		gbc_rdbtnVisa.gridy = 5;
+		gbc_rdbtnVisa.gridy = 7;
 		add(rdbtnVisa, gbc_rdbtnVisa);
 		
 		JRadioButton rdbtnMastercard = new JRadioButton("MasterCard");
@@ -90,33 +86,57 @@ public class AgregarClientePanel extends JPanel {
 		GridBagConstraints gbc_rdbtnMastercard = new GridBagConstraints();
 		gbc_rdbtnMastercard.insets = new Insets(0, 0, 5, 5);
 		gbc_rdbtnMastercard.gridx = 1;
-		gbc_rdbtnMastercard.gridy = 5;
+		gbc_rdbtnMastercard.gridy = 7;
 		add(rdbtnMastercard, gbc_rdbtnMastercard);
 		
 		JRadioButton rdbtnAmericanExpress = new JRadioButton("American Express");
 		btnGroupTarjetas.add(rdbtnAmericanExpress);
 		GridBagConstraints gbc_rdbtnAmericanExpress = new GridBagConstraints();
+		gbc_rdbtnAmericanExpress.anchor = GridBagConstraints.WEST;
 		gbc_rdbtnAmericanExpress.insets = new Insets(0, 0, 5, 5);
 		gbc_rdbtnAmericanExpress.gridx = 2;
-		gbc_rdbtnAmericanExpress.gridy = 5;
+		gbc_rdbtnAmericanExpress.gridy = 7;
 		add(rdbtnAmericanExpress, gbc_rdbtnAmericanExpress);
 		
 		JLabel lblNumeroDeTarjeta = new JLabel("Numero de Tarjeta");
 		GridBagConstraints gbc_lblNumeroDeTarjeta = new GridBagConstraints();
-		gbc_lblNumeroDeTarjeta.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNumeroDeTarjeta.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNumeroDeTarjeta.anchor = GridBagConstraints.EAST;
 		gbc_lblNumeroDeTarjeta.gridx = 0;
-		gbc_lblNumeroDeTarjeta.gridy = 7;
+		gbc_lblNumeroDeTarjeta.gridy = 9;
 		add(lblNumeroDeTarjeta, gbc_lblNumeroDeTarjeta);
 		
 		tFNumeroTarjeta = new JTextField();
 		GridBagConstraints gbc_tFNumeroTarjeta = new GridBagConstraints();
-		gbc_tFNumeroTarjeta.insets = new Insets(0, 0, 0, 5);
+		gbc_tFNumeroTarjeta.insets = new Insets(0, 0, 5, 5);
 		gbc_tFNumeroTarjeta.fill = GridBagConstraints.HORIZONTAL;
 		gbc_tFNumeroTarjeta.gridx = 1;
-		gbc_tFNumeroTarjeta.gridy = 7;
+		gbc_tFNumeroTarjeta.gridy = 9;
 		add(tFNumeroTarjeta, gbc_tFNumeroTarjeta);
 		tFNumeroTarjeta.setColumns(10);
+		
+		JButton btnAceptar = new JButton("Aceptar");
+		GridBagConstraints gbc_btnAceptar = new GridBagConstraints();
+		gbc_btnAceptar.anchor = GridBagConstraints.EAST;
+		gbc_btnAceptar.insets = new Insets(0, 0, 0, 5);
+		gbc_btnAceptar.gridx = 0;
+		gbc_btnAceptar.gridy = 11;
+		add(btnAceptar, gbc_btnAceptar);
+		
+		JButton btnCancelar = new JButton("Cancelar");
+		GridBagConstraints gbc_btnCancelar = new GridBagConstraints();
+		gbc_btnCancelar.insets = new Insets(0, 0, 0, 5);
+		gbc_btnCancelar.gridx = 1;
+		gbc_btnCancelar.gridy = 11;
+		add(btnCancelar, gbc_btnCancelar);
+		
+		JButton btnSalir = new JButton("Salir");
+		GridBagConstraints gbc_btnSalir = new GridBagConstraints();
+		gbc_btnSalir.anchor = GridBagConstraints.WEST;
+		gbc_btnSalir.insets = new Insets(0, 0, 0, 5);
+		gbc_btnSalir.gridx = 2;
+		gbc_btnSalir.gridy = 11;
+		add(btnSalir, gbc_btnSalir);
 
 	}
 
--- src/vistas/principal/ClientePanel.java
+++ src/vistas/principal/ClientePanel.java
@@ -16,17 +16,25 @@ public class ClientePanel extends JPanel {
 		setBackground(Color.WHITE);
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{63, 0, 0, 0, 45, 0};
-		gridBagLayout.rowHeights = new int[]{73, 0, 59, 0, 0};
+		gridBagLayout.rowHeights = new int[]{58, 44, 0, 59, 0, 0};
 		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
+		
+		JLabel lblCliente = new JLabel("Cliente");
+		GridBagConstraints gbc_lblCliente = new GridBagConstraints();
+		gbc_lblCliente.gridwidth = 2;
+		gbc_lblCliente.insets = new Insets(0, 0, 5, 5);
+		gbc_lblCliente.gridx = 2;
+		gbc_lblCliente.gridy = 1;
+		add(lblCliente, gbc_lblCliente);
 		
 		JLabel lblEmail = new JLabel("E-mail");
 		GridBagConstraints gbc_lblEmail = new GridBagConstraints();
 		gbc_lblEmail.insets = new Insets(0, 0, 5, 5);
 		gbc_lblEmail.anchor = GridBagConstraints.EAST;
 		gbc_lblEmail.gridx = 1;
-		gbc_lblEmail.gridy = 1;
+		gbc_lblEmail.gridy = 2;
 		add(lblEmail, gbc_lblEmail);
 		
 		tFEmail = new JTextField();
@@ -35,7 +43,7 @@ public class ClientePanel extends JPanel {
 		gbc_tFEmail.insets = new Insets(0, 0, 5, 5);
 		gbc_tFEmail.fill = GridBagConstraints.HORIZONTAL;
 		gbc_tFEmail.gridx = 2;
-		gbc_tFEmail.gridy = 1;
+		gbc_tFEmail.gridy = 2;
 		add(tFEmail, gbc_tFEmail);
 		tFEmail.setColumns(10);
 		
@@ -44,28 +52,28 @@ public class ClientePanel extends JPanel {
 		GridBagConstraints gbc_lblErrorEmail = new GridBagConstraints();
 		gbc_lblErrorEmail.insets = new Insets(0, 0, 5, 0);
 		gbc_lblErrorEmail.gridx = 4;
-		gbc_lblErrorEmail.gridy = 1;
+		gbc_lblErrorEmail.gridy = 2;
 		add(lblErrorEmail, gbc_lblErrorEmail);
 		
 		JButton btnAceptar = new JButton("Aceptar");
 		GridBagConstraints gbc_btnAceptar = new GridBagConstraints();
 		gbc_btnAceptar.insets = new Insets(0, 0, 0, 5);
 		gbc_btnAceptar.gridx = 1;
-		gbc_btnAceptar.gridy = 3;
+		gbc_btnAceptar.gridy = 4;
 		add(btnAceptar, gbc_btnAceptar);
 		
 		JButton btnCancelar = new JButton("Cancelar");
 		GridBagConstraints gbc_btnCancelar = new GridBagConstraints();
 		gbc_btnCancelar.insets = new Insets(0, 0, 0, 5);
 		gbc_btnCancelar.gridx = 2;
-		gbc_btnCancelar.gridy = 3;
+		gbc_btnCancelar.gridy = 4;
 		add(btnCancelar, gbc_btnCancelar);
 		
 		JButton btnNuevoCliente = new JButton("Nuevo Cliente");
 		GridBagConstraints gbc_btnNuevoCliente = new GridBagConstraints();
 		gbc_btnNuevoCliente.insets = new Insets(0, 0, 0, 5);
 		gbc_btnNuevoCliente.gridx = 3;
-		gbc_btnNuevoCliente.gridy = 3;
+		gbc_btnNuevoCliente.gridy = 4;
 		add(btnNuevoCliente, gbc_btnNuevoCliente);
 
 	}
